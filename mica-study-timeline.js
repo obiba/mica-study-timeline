72995f946ca2519535cc5ce4bc5716ee66888b71
Ramin Haeri Azad <rhaeri@p3g.org>
2014-05-01 18:12:20 -0400

    new-data-format - prepared the DTO to complete data transformation

--- mica-study-timeline.js
+++ mica-study-timeline.js
@@ -320,28 +320,49 @@
   $.MicaTimeline.prototype = {
 
     create: function (selectee, timelineData) {
-      var width = $(selectee).width();
-      var chart = d3.timeline()
-        .startYear(timelineData.start)
-        .beginning(timelineData.min)
-        .ending(timelineData.max)
-        .width(width)
-        .stack()
-        .tickFormat({
-          format: d3.format("d"),
-          tickTime: 1,
-          tickNumber: 1,
-          tickSize: 10
-        })
-        .margin({left: 15, right: 15, top: 0, bottom: 20})
-        .rotateTicks(timelineData.max > this.maxMonths ? 45 : 0)
-        .click(function (d, i, datum) {
-          $('#event-' + d.id).modal();
-        });
+      trasformData(timelineData);
 
-      d3.select(selectee).append("svg").attr("width", width).datum(timelineData.data).call(chart);
+//      var width = $(selectee).width();
+//      var chart = d3.timeline()
+//        .startYear(timelineData.start)
+//        .beginning(timelineData.min)
+//        .ending(timelineData.max)
+//        .width(width)
+//        .stack()
+//        .tickFormat({
+//          format: d3.format("d"),
+//          tickTime: 1,
+//          tickNumber: 1,
+//          tickSize: 10
+//        })
+//        .margin({left: 15, right: 15, top: 0, bottom: 20})
+//        .rotateTicks(timelineData.max > this.maxMonths ? 45 : 0)
+//        .click(function (d, i, datum) {
+//          $('#event-' + d.id).modal();
+//        });
+//
+//      d3.select(selectee).append("svg").attr("width", width).datum(timelineData.data).call(chart);
     }
+
   };
+
+  function trasformData(timelineData) {
+    $.each(timelineData.populations, function (i, population) {
+      $.each(population, function (field, value) {
+        switch (field) {
+        case ('id'):
+          console.log("id:", value);
+          break;
+        case ('name'):
+          console.log("name:", value[0].value);
+          break;
+        case ('dataCollectionEvents'):
+          console.log("id:", value);
+          break;
+        }
+      });
+    });
+  }
 
   $.MicaTimeline.defaultOptions = {
     maxMonths: 300
