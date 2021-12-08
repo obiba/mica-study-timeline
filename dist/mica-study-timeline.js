4b1a5bc9b354587b58f6bab51d56215ed314265e
Ramin Haeri Azad <rhaeri@maelstrom-reseach.org>
2021-12-08 16:53:30 -0500

    Prepared release 1.1.2 and updated dist/

--- dist/mica-study-timeline.js
+++ dist/mica-study-timeline.js
@@ -4,8 +4,8 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see  <http://www.gnu.org/licenses>
 
-* mica-study-timeline - v1.1.1
-* Date: 2021-11-25
+* mica-study-timeline - v1.1.2
+* Date: 2021-12-08
  */
 (function () {
 
@@ -380,7 +380,7 @@
 
         $.each(population.dataCollectionEvents, function (j, dce) {
           dce.startDate = dce.startDay ? makeDateFromString(dce.startDay) : makeStartDate(dce.startYear, dce.startMonth);
-          dce.endDate = dce.endDay ? makeDateFromString(dce.endDay) : makeEndDate(dce.endYear || currentYear, dce.endMonth);
+          dce.endDate = dce.endDay ? makeDateFromString(dce.endDay) : makeEndDate(dce.endYear || currentYear > dce.startYear ? currentYear : dce.startYear, dce.endMonth);
         });
       }
     });
@@ -398,8 +398,8 @@
     $.each(populations, function (i, population) {
       if (population.hasOwnProperty('dataCollectionEvents')) {
         $.each(population.dataCollectionEvents, function (j, dce) {
-          startYear = Math.min(startYear, dce.startYear);
-          var dceEndYear = dce.endYear ? dce.endYear : new Date().getFullYear();
+          startYear = Math.min(startYear, dce.startDate.getFullYear());
+          var dceEndYear = dce.endDate.getFullYear();
           if (endYear < dceEndYear) {
             endYear = dceEndYear;
           }
