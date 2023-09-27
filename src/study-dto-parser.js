3f8ba1440f1affd09bf4fd83529aab423028f1bc
Ramin Haeri Azad <rhaeri@maelstrom-reseach.org>
2023-09-27 13:48:31 -0400

    Fixed variable

--- src/study-dto-parser.js
+++ src/study-dto-parser.js
@@ -276,7 +276,7 @@
         setTitle(dce, dceDto, 'name');
         setStartingTime(dce, dceDto, bounds);
         setEndingTime(dce, dceDto, bounds);
-        setOnGoing(study, studyDto);
+        setOnGoing(dce, dceDto);
         return dce;
       }
 
